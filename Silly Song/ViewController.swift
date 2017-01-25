3fb4981f068d5b79ed9f9463e48931921d940d1d
Gerardo Camilo <gerardo.camilo@gmail.com>
2017-01-24 20:13:51 -0400

    Silly Song project.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +26,70 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func reset(_ sender: Any) {
+        self.nameField.text = ""
+        self.lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        
+        if let tmpName = self.nameField.text, tmpName != "" {
+            let resultingSong: String = lyricsForName(lyrics: getSongTemplate(), name: tmpName)
+            self.lyricsView.text = resultingSong
+        }
+        
+    }
+    
 }
 
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+func getSongTemplate() -> String {
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joined(separator: "\n")
+    
+    return bananaFanaTemplate
+}
+
+func shortNamefromName(name: String) -> String{
+    var shortName = String()
+    var tmpName = name.lowercased()
+    let vowels: [Character] = ["a","e","i","o","u"]
+    
+    tmpName = tmpName.folding(options: .diacriticInsensitive, locale: .current)
+    
+    var myStartingIndex: Int = 0
+    
+    for (index, letter) in tmpName.characters.enumerated() {
+        if vowels.contains(letter){
+            myStartingIndex = index
+            break
+        }
+    }
+    
+    let index = tmpName.index(tmpName.startIndex, offsetBy: myStartingIndex)
+    shortName = tmpName.substring(from: index)
+    
+    return shortName
+}
+
+func lyricsForName(lyrics: String, name:String) -> String {
+    
+    let shortName = shortNamefromName(name: name)
+    
+    let lyricsTemplate = lyrics
+        .replacingOccurrences(of: "<FULL_NAME>", with: name)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyricsTemplate
+}
